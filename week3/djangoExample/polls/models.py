fba6f47a1163d9a0c6c24474a605dcf204d956ee
1000zoo <cjswldn99@gmail.com>
2024-04-09 00:07:20 +0900

    3주차 - choice 출력 변경

--- week3/djangoExample/polls/models.py
+++ week3/djangoExample/polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'제목: {self.question_text}, 날짜: {self.pub_date}'
@@ -11,3 +11,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'[{self.question.question_text}] {self.choice_text}'

4b51a064feb7f748cc5bd02ccc06e9c918935005
1000zoo <cjswldn99@gmail.com>
2024-04-09 13:55:57 +0900

    3주차 - appname 이용하기

--- week3/djangoExample/polls/urls.py
+++ week3/djangoExample/polls/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path
 
+app_name = "polls" # 실제 app 이름과 달라도 괜찮다.
 urlpatterns = [
     path('', views.index, name="index"),
     path('<int:question_id>/', views.detail, name="detail"),

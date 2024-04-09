7a582117cfba477ee8f4f40c23f85edc77d7d3d5
1000zoo <cjswldn99@gmail.com>
2024-04-09 15:28:56 +0900

    3주차 - admin 페이지 커스터마이징

--- week3/djangoExample/polls/models.py
+++ week3/djangoExample/polls/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
+from django.contrib import admin
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    question_text = models.CharField(max_length=200, verbose_name="질문")
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name="생성일")
 
     def __str__(self):
         return f'제목: {self.question_text}, 날짜: {self.pub_date}'
+    
+    @admin.display(boolean=True, description="최근 생성")
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)

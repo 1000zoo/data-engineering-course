4cca9facb731776bc8165bb9669aca9971ddeca7
1000zoo <cjswldn99@gmail.com>
2024-04-11 14:07:06 +0900

    3주차 - 클래스 기반의 뷰

--- week3/djangoExample/polls_api/views.py
+++ week3/djangoExample/polls_api/views.py
@@ -5,21 +5,44 @@ from polls_api.serializers import *
 
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
-@api_view(['GET', 'POST'])
-def question_list(request):
-    if request.method == 'GET':
+class QuestionList(APIView):
+    def get(self, request):
         questions = Question.objects.all()
         serializer = QuestionSerializer(questions, many=True)
         return Response(serializer.data)
-    
-    if request.method == 'POST':
+
+    def post(self, request):
         serializer = QuestionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET', 'POST'])
+# def question_list(request):
+#     if request.method == 'GET':
+#         questions = Question.objects.all()
+#         serializer = QuestionSerializer(questions, many=True)
+#         return Response(serializer.data)
+    
+#     if request.method == 'POST':
+#         serializer = QuestionSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class QuestionDetail(APIView):
+    def get(self, request, id):
+        pass
+    def put(self, request, id):
+        pass
+    def delete(self, request, id):
+        pass
 
 @api_view(["GET", "PUT", "DELETE"])
 def question_detail(request, id):

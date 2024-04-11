e651d5037758c06be8be6c1f5a86b68f3e3261da
1000zoo <cjswldn99@gmail.com>
2024-04-11 13:36:05 +0900

    3주차 - GET API 만들기

--- week3/djangoExample/polls_api/views.py
+++ week3/djangoExample/polls_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from polls.models import *
+from polls_api.serializers import *
 
-# Create your views here.
+from rest_framework.response import Response
+
+@api_view()
+def question_list(request):
+    questions = Question.objects.all()
+    serializer = QuestionSerializer(questions, many=True)
+    return Response(serializer.data)

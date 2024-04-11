ac5430a8c4b1098eb386e5774b8996d88b574c83
1000zoo <cjswldn99@gmail.com>
2024-04-11 15:07:26 +0900

    3주차 - User 불러오는 api 페이지

--- week3/djangoExample/polls_api/views.py
+++ week3/djangoExample/polls_api/views.py
@@ -1,11 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from rest_framework.decorators import api_view
 from polls.models import *
 from polls_api.serializers import *
-
-from rest_framework.response import Response
-from rest_framework import status, mixins, generics
-from rest_framework.views import APIView
+from rest_framework import generics
+from django.contrib.auth.models import User
 
 class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
@@ -15,3 +11,10 @@ class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer

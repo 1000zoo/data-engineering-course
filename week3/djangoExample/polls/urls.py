dc7bc3a622deb4246df4a0b36b937b862a35600a
1000zoo <cjswldn99@gmail.com>
2024-04-11 15:21:52 +0900

    3주차 - django form을 이용해 회원가입 만들기

--- week3/djangoExample/polls/urls.py
+++ week3/djangoExample/polls/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path
+from .views import SingupView
 
 app_name = "polls" # 실제 app 이름과 달라도 괜찮다.
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     path('<int:question_id>/', views.detail, name="detail"),
     path('<int:question_id>/result', views.result, name="result"),
     path('<int:question_id>/vote/', views.vote, name="vote"),
+    path('signup/', SingupView.as_view(), name='signup')
 ]
--- week3/djangoExample/polls/views.py
+++ week3/djangoExample/polls/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.db.models import F
 
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"questions": latest_question_list}
@@ -27,3 +31,9 @@ def vote(request, question_id):
 def result(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/result.html', {'question': question})
+
+class SingupView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('user-list') ## user를 만든 뒤 특정 url로 보내줌
+    template_name = 'registration/signup.html'
+

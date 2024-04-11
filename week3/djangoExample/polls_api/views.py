227e6f61d44fecd265bc8bb2377407df5dcbc678
1000zoo <cjswldn99@gmail.com>
2024-04-11 14:24:39 +0900

    3주차 - generics 상속으로 더욱 편하게 구현하기

--- week3/djangoExample/polls_api/views.py
+++ week3/djangoExample/polls_api/views.py
@@ -7,32 +7,13 @@ from rest_framework.response import Response
 from rest_framework import status, mixins, generics
 from rest_framework.views import APIView
 
-class QuestionList(mixins.ListModelMixin,
-                   mixins.CreateModelMixin,
-                   generics.GenericAPIView):
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+        
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-        
-class QuestionDetail(mixins.RetrieveModelMixin,
-                     mixins.UpdateModelMixin,
-                     mixins.DestroyModelMixin,
-                     generics.GenericAPIView):
-    queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-        
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
 

3f914431efa5e93e67828b0580e263675e8a0987
1000zoo <cjswldn99@gmail.com>
2024-04-12 18:59:37 +0900

    3주차 - 마무리

--- week3/djangoExample/polls_api/tests.py
+++ week3/djangoExample/polls_api/tests.py
@@ -1,8 +1,42 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.utils import timezone
 
 from polls_api.serializers import *
 from polls.models import *
+
+class QuestionListTest(APITestCase):
+    def setUp(self) -> None:
+        self.question_data = {'question_text': "some question"}
+        self.url = reverse('question-list')
+    
+    def test_create_question(self):
+        user = User.objects.create(username='testuser', password='testpass')
+        self.client.force_authenticate(user=user) ## 사용자 강제 로그인
+        response = self.client.post(self.url, self.question_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Question.objects.count(), 1)
+        question = Question.objects.first()
+        self.assertEqual(question.question_text, self.question_data['question_text'])
+        self.assertLess((timezone.now() - question.pub_date).total_seconds(), 1)
+
+    def test_create_question_without_authentication(self):
+        response = self.client.post(self.url, self.question_data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_list_questions(self):
+        question1 = Question.objects.create(question_text="question1")
+        choice1 = Choice.objects.create(question=question1, choice_text='choice1')
+        question2 = Question.objects.create(question_text="question2")
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(len(response.data), 2)
+        print(f"$$$$$$$$$$  {response.data}")
+
 
 class VoteSerializerTest(TestCase):
     def setUp(self):

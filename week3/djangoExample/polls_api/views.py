c9d9ef239bee57f906fb26ccbe5338fe53fe034b
1000zoo <cjswldn99@gmail.com>
2024-04-12 16:07:12 +0900

    3주차 - validation

--- week3/djangoExample/polls_api/views.py
+++ week3/djangoExample/polls_api/views.py
@@ -1,8 +1,10 @@
 from polls.models import *
 from polls_api.serializers import *
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from django.contrib.auth.models import User
 from .permissions import *
+
+from rest_framework.response import Response
 
 class VoteList(generics.ListCreateAPIView):
     serializer_class = VoteSerializer
@@ -11,13 +13,23 @@ class VoteList(generics.ListCreateAPIView):
     def get_queryset(self, *args, **kwargs):
         return Vote.objects.filter(voter=self.request.user)
     
-    def perform_create(self, serializer):
-        serializer.save(voter=self.request.user)
+    def create(self, request, *args, **kwargs):
+        new_data = request.data.copy()
+        new_data['voter'] = request.user.id
+        serializer = self.get_serializer(data=new_data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 class VoteDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
     permission_classes = [permissions.IsAuthenticated, IsVoter]
+
+    def perform_update(self, serializer):
+        serializer.save(voter=self.request.user)
 
 class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
